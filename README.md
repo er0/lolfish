72ef387cfe89dcf893232b2bdf691b32d0fca941
er0 <erxero@gmail.com>
2014-06-13 19:16:34 -0600

    add install note in README and tmux session count to right prompt

--- README.md
+++ README.md
@@ -21,10 +21,18 @@ You will not need any special patched fonts for this to work.
  * Flags:
      * Current project's Git branch: (master)
      * Number of dirty files in the Git project: (master:3)
-     * Background jobs: &[n]
-     * Exit status of previous command: !![n]
+     * Background jobs: &:n
+     * Exit status of previous command: !:n
+     * Number of tmux sessions:  t:n
+     * The date in tmux format:  Hour:Min Day-Month-Year
      * You currently are a normal user: %
      * You currently are root: #
+
+### Install
+  * source both the fish_prompt.fish and fish_right_prompt.fish
+    files from your $HOME/.config/fish/config.fish startup file.
+
+    
 
 [screenshot1]: http://i.imgur.com/InJELf3.png
 [screenshot2]: http://i.imgur.com/v6aI9AB.png
--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -1,25 +1,41 @@
+
+#
+# Right prompt
+#
 function fish_right_prompt
 
-        #
-        # store the previous command return status for later
-        #
-        set -l exit_status $status
-
-        #
-        # get the number of background jobs
-        #
-        set -l jobs (count (jobs -p ^/dev/null))
+	#
+	# store the previous command return status for later
+	#
+	set -l exit_status $status
 
 	#
-        # when a command errors, display the return value
-	# of the last command !![exit_status]
-        #
-        test $exit_status -ne 0; and set -l error '!' '[' $exit_status ']'
+	# get the number of background jobs
+	#
+	set -l jobs (count (jobs -p ^/dev/null))
 
-        #
-        # display the number of background jobs &[jobs]
-        #
-        test $jobs -ne 0; and set -l bjobs ' ' '&' '[' $jobs ']'
+	#
+	# when a command errors, display the return value
+	# of the last command, !:exit_status
+	#
+	test $exit_status -ne 0; and set -l error '!' ':' $exit_status ' '
 
-	lolfish $error $bjobs
+	#
+	# display the number of background jobs, &:jobs
+	#
+	test $jobs -ne 0; and set -l bjobs '&' ':' $jobs ' '
+
+	# display the number of tmux sessions, t:tmux_sessions
+	# but not in the tmux window
+	if not test $TMUX
+		set -l tmux_sessions (count (tmux list-sessions ^/dev/null))
+		if test $tmux_sessions -gt 0
+			set tmux 't' ':' $tmux_sessions ' '
+		end
+		# date in default tmux format, [Hour:Minute Day-Month-Year]
+		set date (date +'%H:%M %d-%m-%Y' ^/dev/null)
+	end
+
+
+	lolfish $error $bjobs $tmux $date
 end
