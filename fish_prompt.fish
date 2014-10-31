8c64de4d52ad791129a3dd8b8cab5a50bf187252
er0 <erxero@gmail.com>
2014-10-31 03:56:10 -0600

    mostly unnecessary

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,32 +1,34 @@
-# name:		lolfish
-# requires:	jobs, git, hostname, sed
-# inspirationk:	lolcat
+# prompt name: lolfish
+# prompt requires: jobs, git, hostname, sed
 
-# set the seed color to start the prompt
-# 1 starts at red
-# 6 starts at yellow
-# 16 starts at green
-# 21 starts at blue
-# 26 starts at magenta
-
-set start_color 21
+#
+# set the beginning color as a env variable so
+# the next prompt will continue the color sequence
+#  1 : red
+#  6 : yellow
+# 16 : green
+# 21 : blue
+# 26 : magenta
+#
+set start_color 1
 
 function lolfish -d "very rainbow. wow"
 
 	#
-	# valid xterm-256color r g b color space hex values
-	# r g b can be any of 00, 57, 87, af, d7, ff
+	# xterm-256color RGB color values
+	# valid R G B hex values : 00, 57, 87, af, d7, ff
+    #
 	# red    ff0000
 	# yellow ffff00
 	# green  00ff00
 	# blue   0000ff
 	#
 	set -l colors 	ff0000 ff5700 ff8700 ffaf00 ffd700 \
-			ffff00 d7ff00 afff00 87ff00 57ff00 \
-			00ff00 00ff57 00ff87 00ffaf 00ffd7 \
-			00ffff 00d7ff 00afff 0087ff 0057ff \
-			0000ff 5700ff 8700ff af00ff d700ff \
-			ff00ff ff00d7 ff00af ff0087 ff0057
+                    ffff00 d7ff00 afff00 87ff00 57ff00 \
+                    00ff00 00ff57 00ff87 00ffaf 00ffd7 \
+                    00ffff 00d7ff 00afff 0087ff 0057ff \
+                    0000ff 5700ff 8700ff af00ff d700ff \
+                    ff00ff ff00d7 ff00af ff0087 ff0057
 
 	#
 	# set the color differential between prompt items
@@ -35,15 +37,15 @@ function lolfish -d "very rainbow. wow"
 	# 10 has an interesting property
 	# 5 works best for non 256 color terminals
 	#
-	set -l next_color 1
+	set -l color_step 1
 
 	#
-	# set the start color or default to blue
+	# set the starting color or default to the first in the color list
 	#
 	if test $start_color -gt 0
 		set color $start_color
 	else
-		set color 21
+		set color 1
 	end
 
 	#
@@ -66,7 +68,7 @@ function lolfish -d "very rainbow. wow"
 		end
 
 		#
-		# print these symbols in normal color
+		# print these characters in normal color
 		#
 		switch $arg
 			case '(' ')' '[' ']' ':' '@' ' '
@@ -76,52 +78,60 @@ function lolfish -d "very rainbow. wow"
 		end
 
 		#
-		# print rainbow!
+		# rainbow! wow
 		#
 		set_color $colors[$color]
 		echo -n -s $arg
-		set color (math $color+$next_color)
+		set color (math $color + $color_step)
 
 	end
 
+    #
 	# capture the next color to use for the start the next line
-	set start_color (math $start_color+$next_color)
+    #
+	set start_color (math $start_color + $color_step)
 
 	set_color normal
 end
 
+
 function fish_prompt
 
 	#
-	# set the user, short hostname (non-fully qualified domain name)
-	# and current path (abbreviated home directory ~ ) in the standard
-	# ssh style format user@hostname:path
+	# last command had an error? display the return value
 	#
-	set -l uname '[' $USER '@'
-	set -l hname (hostname | sed 's/\..*//' ^/dev/null) ':'
-	set -l cwd   (echo $PWD | sed "s,$HOME,~," ^/dev/null) ']' ' '
+    set -l exit_status $status
+	test $exit_status -ne 0; and set -l error '(' $exit_status ')' ' '
 
 	#
-	# the git bits
+	# abbreviated home directory ~
 	#
-	if set -l branch (git rev-parse --abbrev-ref HEAD ^/dev/null)
-		set -l git_dirt (count (git status -s --ignore-submodules ^/dev/null))
-		test $git_dirt -ne 0; and set -l dirty ':' $git_dirt
-		set git 'git' '(' $branch $dirty ')' ' '
+	set -l cwd (echo $PWD | sed -e "s,$HOME,~," 2>/dev/null)
+
+	#
+	# the git stuff
+	#
+	if set -l git_branch (git rev-parse --abbrev-ref HEAD 2>/dev/null)
+		set -l git_dirt (count (git status -s --ignore-submodules 2>/dev/null))
+		test $git_dirt -gt 0; and set -l dirty ':' $git_dirt
+		set git ' git' '[' $git_branch $dirty ']'
 	end
 
 	#
-	# hashtag the prompt for root
+	# hashtag the prompt for root user
 	#
 	switch $USER
 		case 'root'
 			set prompt '#' ' '
 		case '*'
-			set prompt '%' ' '
+			set prompt '>' '>' ' '
 	end
 
 	#
 	# finally print the prompt
 	#
-	lolfish $uname $hname $cwd $git $prompt
+	lolfish (hostname -s) ':' $cwd $git ' ' $error $prompt
+
+    # alternate prompt using ssh style username@hostname:path
+	#lolfish '[' $USER '@' (hostname -s) ':' $cwd ']' $git ' ' $error $prompt
 end
--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -1,41 +1,25 @@
-
 #
-# Right prompt
+# the right side prompt
 #
 function fish_right_prompt
 
 	#
-	# store the previous command return status for later
+	# background jobs
 	#
-	set -l exit_status $status
+	set -l bjobs (count (jobs -p 2>/dev/null))
+	test $bjobs -ne 0; and set -l background_jobs '[' '&' ':' $bjobs ']'
 
-	#
-	# get the number of background jobs
-	#
-	set -l jobs (count (jobs -p ^/dev/null))
-
-	#
-	# when a command errors, display the return value
-	# of the last command, !:exit_status
-	#
-	test $exit_status -ne 0; and set -l error '!' ':' $exit_status ' '
-
-	#
-	# display the number of background jobs, &:jobs
-	#
-	test $jobs -ne 0; and set -l bjobs '&' ':' $jobs ' '
-
-	# display the number of tmux sessions, t:tmux_sessions
-	# but not in the tmux window
+    #
+	# display the date and number of tmux sessions
+	# but not inside the tmux session
+    #
 	if not test $TMUX
-		set -l tmux_sessions (count (tmux list-sessions ^/dev/null))
-		if test $tmux_sessions -gt 0
-			set tmux 't' ':' $tmux_sessions ' '
+		if test (count (tmux list-sessions 2>/dev/null)) -gt 0
+			set tmux '[' '&' ':' 'tmux' ']'
 		end
 		# date in default tmux format, [Hour:Minute Day-Month-Year]
-		set date (date +'%H:%M %d-%m-%Y' ^/dev/null)
+		set date '[' (date +'%H:%M %d-%m-%Y' 2>/dev/null) ']'
 	end
 
-
-	lolfish $error $bjobs $tmux $date
+	lolfish $error $background_jobs $tmux $date
 end
