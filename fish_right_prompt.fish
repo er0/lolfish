2aeed0308ea0b5fb8ad19534a0240e589646b009
Eric Roseman <eric.roseman@colorado.edu>
2016-01-20 20:59:34 -0700

    check commands before running. git status now shows in sub directories. various code cleanup.

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -1,26 +1,41 @@
+# Right side prompt
 #
-# the right side prompt
+# Requires the lolfish function provided in lolfish:fish_prompt.fish source
 #
+# Displays the number of background processes [&:2]
+# Displays the number of active tmux sessions [tmux:4]
+# Displays the time and date in Minute:Hour Day:Month:Year
+
 function fish_right_prompt
 
-	#
-	# background jobs
-	#
-	set -l bjobs (count (jobs -p 2>/dev/null))
-	test $bjobs -ne 0; and set -l background_jobs '[' '&' ':' $bjobs ']'
+    #
+    # background jobs
+    #
+    set -l background_jobs (count (jobs -p ^/dev/null))
+    if test $background_jobs -gt 0
+        set background_jobs_prompt '[' '&' ':' $background_jobs ']'
+    end
 
-	#
-	# display the date and number of tmux sessions
-	# but not inside the tmux session
-	#
-	if test -z $TMUX
-		set -l tmux_sessions (count (tmux list-sessions 2>/dev/null))
-		if test $tmux_sessions -gt 0
-			set tmux '[' 'tmux' ':' $tmux_sessions ']'
-		end
-		# date in default tmux format, [Hour:Minute Day-Month-Year]
-		set date '[' (date +'%H:%M %d-%m-%Y' 2>/dev/null) ']'
-	end
+    #
+    # Display the number of background tmux sessions
+    # only if the shell is running outside of tmux
+    #
+    if test -z $TMUX
+        if command -s tmux > /dev/null ^&1
+            set -l tmux_sessions (count (tmux list-sessions ^/dev/null))
+            if test $tmux_sessions -gt 0
+                set tmux_sessions_prompt '[' 'tmux' ':' $tmux_sessions ']'
+            end
+        end
+    end
 
-	lolfish $background_jobs $tmux $date
+    #
+    # Display the time and date
+    #
+    if command -s date > /dev/null ^&1
+        set time (date +'%H:%M' ^/dev/null)
+        set date (date +'%d-%m-%Y' ^/dev/null)
+    end
+
+    lolfish $background_jobs_prompt $tmux_sessions_prompt $time ' ' $date
 end
